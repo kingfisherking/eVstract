2363340681827dc47264acc725dcca7c47e9c412
kingfisherking <Quaaysan@gmail.com>
2023-11-12 22:56:54 -0600

    Updating jquery, test, and manifest

--- background.js
+++ background.js
@@ -1,6 +1,6 @@
 chrome.action.onClicked.addListener((tab) => {
     chrome.scripting.executeScript({
         target: {tabId: tab.id},
-        files: ['eVstract.js']
+        files: ['test.js']
     });
 });
---
+++ test.js
@@ -0,0 +1,6 @@
+// this is a test for the jquery
+$(document).ready(()=>{
+    const $allData = $(document).html();
+    var newTab = window.open("hello world", "_blank")
+
+})
